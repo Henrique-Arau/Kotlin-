bf9f54a279792afca3c2e34cf7deac31ebe52c72
Henrique-Arau <penielh43@gmail.com>
2022-01-16 02:16:12 -0300

    inner class Endereço

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -5,6 +5,11 @@ class Pessoa {
     var nome: String = "Caio"
     var cpf: String = "123.123.123.11"
 
+
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
+
 }
 
 fun main() {
@@ -13,4 +18,6 @@ fun main() {
 
     println(caio.nome)
     println(caio.cpf)
+
+    println(caio.Endereco().rua)
 }
